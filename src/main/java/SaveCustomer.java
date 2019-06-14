42ea9f1184aa26d9eb91b0bdc88d8cdf069444f2
Prize-winner <3220016928@qq.com>
2019-06-14 23:59:43 +0800

    Intellij创建简单的maven小demo

---
+++ src/main/java/SaveCustomer.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 宁三水
+ * @version 1.0
+ * @descript PACKAGE_NAME
+ * @date 2019/6/10
+ */
+
+
+public class SaveCustomer  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+
+        String name = request.getParameter("name");
+
+        System.out.println(name+"---------------");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/6/10
+  Time: 11:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<form action="SaveCustomer" method="post">
+    姓名：<input type="text" name="name"><br>
+    <input type="submit" value="提交">
+</form>
+
+</body>
+</html>
